edfb4c23947e6ba89f44228a35d81c78c45e6051
geremy22 <geremysexmagik@gmail.com>
2016-06-08 16:12:15 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var fs = require('fs'),
+	p = require('path'),
+	u = require('util');
+
+function doFilter(filters,path,stat){
+	return filters.every((f)=>{
+		if(u.isFunction(f))
+			return f(path,stat);
+		if(u.isRegExp(f))
+			return f.test(path);
+		return true;
+	});
+}	
+
+function rreaddirSync(path, pfilters, plist) {
+  var list = [], filters = u.isArray(pfilters) ? pfilters : [pfilters],
+	files = fs.readdirSync(path);
+
+	files.forEach(function (file) {
+		var subPath = p.join(path, file),
+			stats = fs.lstatSync(subPath);
+		doFilter(filters,subPath,stats) && list.push(subPath);
+		stats.isDirectory() && recursiveReaddirSync(subPath, filters, list);
+	});
+
+  return list;
+}
+
+rreaddirSync.ONLY_DIR = (p,s)=>s.isDirectory();
+rreaddirSync.ONLY_FILE = (p,s)=>s.isFile();
+
+module.exports = rreaddirSync;
