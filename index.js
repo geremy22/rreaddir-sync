fc4bb575bd8a941777cff302eb33276ad3398bad
German Velazquez <germanmanuelvelazquez@gmail.com>
2016-06-10 18:27:14 -0300

    Added depth feature and subbdir exclusion

--- index.js
+++ index.js
@@ -4,29 +4,34 @@ var fs = require('fs'),
 
 function doFilter(filters,path,stat){
 	return filters.every((f)=>{
-		if(u.isFunction(f))
-			return f(path,stat);
-		if(u.isRegExp(f))
-			return f.test(path);
-		return true;
+		(u.isRegExp(f)) && console.log("Path: " + path + " RegExp: " + f + " RES: " + f.test(path));
+		return (u.isFunction(f))
+			? f(path,stat) :
+		(u.isRegExp(f)) 
+			? f.test(path) :
+		true;
 	});
 }	
 
-function rreaddirSync(path, pfilters, plist) {
+function rreaddirSync(path, pfilters, depth, plist, pcurrDepth) {
   var list = plist || [], filters = u.isArray(pfilters) ? pfilters : [pfilters],
-	files = fs.readdirSync(path);
+	files = fs.readdirSync(path), currDepth = u.isNumber(pcurrDepth) ? pcurrDepth : 1;
 
 	files.forEach(function (file) {
 		var subPath = p.join(path, file),
 			stats = fs.lstatSync(subPath);
-		doFilter(filters,subPath,stats) && list.push(subPath);
-		stats.isDirectory() && rreaddirSync(subPath, filters, list);
+		if(doFilter(filters,subPath,stats)){
+			list.push(subPath);
+			(!u.isNumber(depth) || currDepth < depth) && 
+			stats.isDirectory() && rreaddirSync(subPath, filters, depth, list, currDepth + 1);
+		}
 	});
-
   return list;
 }
 
-rreaddirSync.ONLY_DIR = (p,s)=>s.isDirectory();
-rreaddirSync.ONLY_FILE = (p,s)=>s.isFile();
+var exp = (p,f,l,d)=>rreaddirSync(p,f,d,l);
 
-module.exports = rreaddirSync;
+exp.ONLY_DIR = (p,s)=>s.isDirectory();
+exp.ONLY_FILE = (p,s)=>s.isFile();
+
+module.exports = exp;
